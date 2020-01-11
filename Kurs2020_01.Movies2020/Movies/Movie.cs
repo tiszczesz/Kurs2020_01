a85287afb92de0ac01171453324921a8e058420e
Pawe Szczeszek <pszczesz@gmail.com>
2020-01-11 13:01:15 +0100

    d2

--- Kurs2020_01.Movies2020/Movies/Movie.cs
+++ Kurs2020_01.Movies2020/Movies/Movie.cs
@@ -9,12 +9,21 @@ namespace Kurs2020_01.Movies2020.Movies
     public class Movie
     {
         public int Id { get; set; }
+
+        [Display(Name = "Tytuł")]
+        [Required(ErrorMessage = "Podaj tytuł")]
         public string Title { get; set; }
+
+        [Display(Name = "Rodzaj filmu")]
+        [Required(ErrorMessage = "Podaj rodzaj filmu")]
         public string Genre { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Data realizacji")]
         public DateTime ReleaseDate { get; set; }
 
+
+        [Display(Name = "Cena")]
         public decimal Price { get; set; }
     }
 }
