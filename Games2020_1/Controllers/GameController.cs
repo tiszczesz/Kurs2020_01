9c2247aa5aa49ec86068df4a2dad58897fd8e11d
Pawe Szczeszek <pszczesz@gmail.com>
2020-01-25 12:58:01 +0100

    d2

--- Games2020_1/Controllers/GameController.cs
+++ Games2020_1/Controllers/GameController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Games2020_1.Data;
+using Games2020_1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Games2020_1.Controllers
 {
     public class GameController : Controller
     {
+        private GameContexDb _db;
+
+        public GameController(GameContexDb db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Game> games = _db.Games
+                     .Include(g=>g.Genre).ToList();
+            return View(games);
         }
     }
 }
--- Games2020_1/Program.cs
+++ Games2020_1/Program.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Games2020_1.Data;
+using Games2020_1.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Games2020_1
@@ -14,7 +18,23 @@ namespace Games2020_1
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<GameContexDb>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Error!!");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
